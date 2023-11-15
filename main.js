17e9ad38c09a17b70a15284b3914ae3e7314f9a6
GhynAlly <codeghall92@gmail.com>
2023-11-15 15:05:59 +0300

    h

--- main.js
+++ main.js
@@ -6,18 +6,23 @@ const port = 4000;
 
 var image_data
 
+var snapshot = false
+
 app.post('/upload', (req, res) => {
-    console.log(req.files)
+    console.log("upload")
     var read = req.files;
     image_data = read.media.data;
     res.send()
+    snapshot = false
 });
-
 
 app.get('/', (req, res) => {
-    
+    console.log("/")
     res.send(image_data);
 });
+
+
+
 
 
 app.listen(port, () => {
